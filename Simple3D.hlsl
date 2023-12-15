ec72ee6e403839f044ca2fa1b323ddda84ac4dc5
悠人 菅原 <220369@jc-21.jp>
2023-12-15 11:02:29 +0900

    //2023/12/15

--- Simple3D.hlsl
+++ Simple3D.hlsl
@@ -82,7 +82,7 @@ float4 PS(VS_OUT inData) : SV_Target
     float4 ambient;
 
 	//ãæñ îΩéÀìKópèàóù
-	float4 NL = dot(inData.normal, normalize(lightPosition));
+	//float4 NL = dot(inData.normal, normalize(lightPosition));
 	//float4 reflect = normalize(2 * NL * inData.normal - normalize(lightPosition));
 	float4 r = reflect(normalize(-lightPosition), inData.normal);
 	float4 specular = pow(saturate(dot(r, normalize(inData.eyev))), shininess) * specularColor;
